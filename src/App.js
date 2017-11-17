15661e9bca6e403b0fba1b08b1a2680d968087dd
Hugo Mendoza <cronoshimura@yahoo.es>
2017-11-16 23:22:04 -0500

    avance ejercicio v0.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Header, Image, Modal } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header>
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Modal trigger={<Button>Show Modal</Button>}>
+          <Modal.Header>Select a Photo</Modal.Header>
+          <Modal.Content image>
+          <Image wrapped size='medium' src='/assets/images/avatar/large/rachel.png' />
+        <Modal.Description>
+        <Header>Default Profile Image</Header>
+        <p>We've found the following gravatar image associated with your e-mail address.</p>
+        <p>Is it okay to use this photo?</p>
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
       </div>
     );
   }
