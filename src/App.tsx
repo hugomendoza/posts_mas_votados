541ba483173f8c974248fe5cde5ee6d0e19da199
Hugo Mendoza <hugo_mendoza_s@outlook.com>
2023-11-18 21:54:21 -0500

    feat: migrate from CRA to Vite

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button"
+// import BtnOrder from './components/BtnOrder';
+// import { Template } from './components/Template';
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Blog Posts Populares</h1>
+        </div>
+      </div>
+      <Button>Click Me</Button>
+      {/* <BtnOrder />
+      <Template /> */}
+    </div>
+  );
+}
+
+export default App;
